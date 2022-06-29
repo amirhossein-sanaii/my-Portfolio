74603cec3d920ade508e2e3495efc731f7f8731a
amirhossein <amirhosseinsanaii76@gmail.com>
2022-06-29 16:17:45 +0430

    create v1-about-page

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  try {
+    TagCanvas.Start('myCanvas','tags',{
+      textColour: '#08fdd8',
+      outlineColour: '#1d1d1d',
+      reverse: true,
+      depth: 0.8,
+      maxSpeed: 0.05
+    });
+  } catch(e) {
+    // something went wrong, hide the canvas container
+    document.getElementById('myCanvasContainer').style.display = 'none';
+  }
+};
